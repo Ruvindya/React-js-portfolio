aa815578ed243a79688ebc848c31fab81ed5ba75
Ruvindya <2019e117@eng.jfn.ac.lk>
2023-09-14 14:55:27 +0530

    AboutMe

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,16 @@
+import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
+import Myportfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
+
 
 export default function Home () {
     return (
         <>
             <HeroSection/>
             <MySkills />
+            <AboutMe />
+            <Myportfolio />
         
         
         </>
---
+++ src/Pages/Home/MyPortfolio.jsx
@@ -0,0 +1,13 @@
+import data from "../../data/index.json";
+
+export default function Myportfolio() {
+    return (
+        <section className="portfolio--section" id="MyPortfolio">
+            <div className="portfolio--container-box">
+                <div className="portfolio--container">
+                    <p className="sub--title"> Recent Projects</p>
+                </div>
+            </div>
+        </section>
+    );
+}
